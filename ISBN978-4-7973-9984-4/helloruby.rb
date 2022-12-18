3520cc9f65d755ed85596aca1c277c1e37369491
AyakaKawada <110372008+plyk1022@users.noreply.github.com>
2022-12-18 02:42:10 +0000

    1.1 helloruby

---
+++ ISBN978-4-7973-9984-4/helloruby.rb
@@ -0,0 +1,13 @@
+print("Hello, Ruby. \n")
+print("Hello, RUBY! \n")
+
+print("Hello, \nRuby\n!\n")
+
+print("Hello, \"Ruby\".\n")
+print("Hello, \\ Ruby!\n")
+
+print('Hello, \nRuby\n!\n')
+print('Hello, \\ \'Ruby\'.')
+
+puts "Hello, Ruby."
+puts "Hello, ", "Ruby!"
